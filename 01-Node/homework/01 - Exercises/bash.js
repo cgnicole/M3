a4ab93801e2e95d8f53e0d676ef46b97c6606413
cgnicole <nicolesofiacamargogarcia@192.168.0.8>
2023-04-04 21:57:33 -0500

    creacion repositorio

---
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -0,0 +1,18 @@
+const process = require("process");
+const { Z_ASCII } = require("zlib");
+const commands = require("./commands/index.js");
+
+function print(output) {
+  process.stdout.write(output);
+  process.stdout.write("\nprompt >");
+}
+
+function bash() {
+  print("");
+}
+
+bash();
+module.exports = {
+  print,
+  bash,
+};
