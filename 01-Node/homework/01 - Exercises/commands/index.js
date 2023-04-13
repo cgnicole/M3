45981eeea4b88d4ff7a184d53abe841b21951d9f
cgnicole <soficamargo2004@gmail.com>
2023-04-12 21:55:41 -0500

    primer commit

--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -4,53 +4,22 @@ const utils = require("../utils/request");
 //
 const process = require("process");
 
-function pwd(print) {
-  print(process.cwd());
-}
+function pwd(print) {}
 
-function date(print) {
-  print(Date());
-}
+function date(print) {}
 
-function echo(print, args) {
-  print(args);
-}
+function echo(print, args) {}
 
-function ls(print) {
-  fs.readdir(".", (error, files) => {
-    if (error) {
-      throw error;
-    }
-    print(files.join(" "));
-  });
-}
+function ls(print) {}
 
-const printFile = (print, filename, lines) => {
-  fs.readFile(filename, "utf-8", (error, data) => {
-    if (error) throw Error("Hubo un error");
-    !lines && print(data);
-    lines === "head" && print(data.split("\n").slice(0, 8).join("\n"));
-    lines === "tail" && print(data.split("\n").at(-1));
-  });
-};
+const printFile = (print, filename, lines) => {};
 
-function cat(print, args) {
-  printFile(print, args);
-}
+function cat(print, args) {}
 
-function head(print, args) {
-  printFile(print, args, "head");
-}
+function head(print, args) {}
 
-function tail(print, args) {
-  printFile(print, args, "tail");
-}
+function tail(print, args) {}
 
-function curl(print, args) {
-  utils.request(` https://${args}`, (error, response) => {
-    if (error) throw Error("Hubo un error");
-    print(response.data);
-  });
-}
+function curl(print, args) {}
 
-module.exports = { pwd, date, echo, ls, cat, head, tail, curl };
+module.exports = {};
