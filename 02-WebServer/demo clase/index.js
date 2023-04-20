486574e50e0df6af80a7eaaaa8f5628894b3d203
cgnicole <soficamargo2004@gmail.com>
2023-04-20 00:58:36 -0500

    create web server

--- 02-WebServer/demo clase/index.js
+++ 02-WebServer/demo clase/index.js
@@ -1,19 +1,36 @@
 // asi se crea un servidor
 
-const http = require("hhtp");
+const http = require("http");
 
 // el setvidor recibe una callback, la callback es lo que se va a jecutar cuando el servidor reciba una peticon. cad vez que reviba una peticon ejecuta la funcion
 
 // la funcion recibe dos parametros "req, res", estos son dos objetos que respresentan a la request y a la respuesta respectivamente. estos parametros se pueden llamar como queramos pero siempre le temos que poner por buena practica req y res
 
-http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end;
-});
+http
+  .createServer((req, res) => {
+    // cuando hago el req a la / => reponda con holis
+
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("holis");
+
+    // cuando hago el req a la /students => reponda con JSON  con los students
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify([
+        { id: 1, name: "jorge" },
+        { id: 2, name: "nicole" },
+        { id: 3, name: "juan" },
+      ])
+    );
+  })
+  .listen(3001, "localhost");
 
 // ante culquier peticon queremos que se ejecute esa funcion y nosotros estamos preparando una respuesta con un status y el content type.
 
-// res "estamos hablando de la respuesta" createServer "le estamos dando un encabezado que es informacion que le tenemos que dar a la repsuesta para que despues esa infromacion viaje" 200 "es el status code", "content-type" "es el tipo de contentenido que le vamos a mandar al cliente. ej: "vamos a enviar un archivo de txt, un html, un img "
+// res "estamos hablando de la respuesta" createServer "le estamos dando un encabezado que es informacion que le tenemos que dar a la repsuesta para que despues esa infromacion viaje" 200 "es el status code", "content-type" "es el tipo de contentenido que le vamos a mandar al cliente. ej: "vamos a enviar un archivo de txt, un html, un img " res.end "envio la informacion, la respuesta"
+
+// ❌ STATUS
 
 // 200 - todo salio bien
 // 400 - error del cliente "ej: el cliente escribio mal la url, paso mal el usuario y contraseña, alguna infromacion que tenia que manadar no fue presisa o esta incompleta "
